d5360c48789dc1461edee22d394777a7af8d1ff1
Oliver Herzog <50545667+herz08@users.noreply.github.com>
2025-08-17 18:31:07 +0200

    Update create_service.sh

--- create_service.sh
+++ create_service.sh
@@ -8,6 +8,7 @@ Description=Mein Startscript
 After=network.target
 
 [Service]
+WorkingDirectory=/home/pi
 ExecStart=/home/pi/start_infoscreen.sh
 Restart=on-failure
 User=pi
@@ -22,3 +23,7 @@ echo "systemctl deamon-reload und enable wird ausgeführt..."
 sudo systemctl daemon-reload
 sudo systemctl enable startscreen.service
 echo "Service wurde aktiviert."
+sudo systemctl start startscreen.service
+echo "Service wurde gestartet."
+echo "Status des Services:"
+sudo systemctl status startscreen.service
